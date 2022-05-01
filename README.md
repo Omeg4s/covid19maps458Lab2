84bba455c3b82a9bf35df869201cea1e880081d2
znewton-1831937 <znewton@uw.edu>
2022-04-30 22:08:48 -0700

    finished code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# covid19maps458Lab2
+# Mapping Covid 19 in 2020
+This is a project that undertakes web mapping using the library "mapbox" to create two maps that describe the situation with covid 19 in the year 2020. Covid put the world on notice as the pandemic brought everything to a hold for many years. It resulted in millions dying and new policies being created to hopefully prevent something like it happening again. Map1 is a choropleth map that uses color to show how high the covid rates are for each US county. The case rate is calculated as cases per thousand residents. This allows for case numbers to be normailzed based on how high the population is. The second map is a proportional symbols map that uses various sizes of circles to show how high a case count is for each county. The larger the circle, the higher the number of cases for that area. All this was done using data that was acquired from various sources. The COVID-19 case/death data that was used is originally from The New York Times. The data includes all the cases in 2020. The population data used for calculating the case rates are from the 2018 ACS 5 year estimates. Both data are at the county level. The U.S. county boundary shapefile was downloaded from the U.S. Census Bureau. 
+
+## Map1, Choropleth Map of Covid-19 Rates
+<img src="img/map1sc.png" alt="map 1 screenshot"/>
+
+### Link to the map
+
+## Map2, Proportional Symbol Map of Covid-19 Cases
+<img src="img/map2sc.png" alt="map 2 screenshot"/>
+
+### Link to the map:
--- js/main.js
+++ js/main.js
@@ -1,103 +1,88 @@
 mapboxgl.accessToken =
 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+
 let map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/dark-v10',
-zoom: 4.5, // starting zoom
-center: [-101, 40] // starting center
-});
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 4.5, // starting zoom
+    center: [-100, 40] // starting center
+    });
 
-const grades = [5000, 10000, 20000, 30000],
-colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)', 'rgb(179, 0, 65)']
-radii = [2, 5, 15, 20];
+    map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
 
-//load data to the map as new layers.
-//map.on('load', function loadingData() {
-map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
-
-// when loading a geojson, there are two steps
-// add a source of the data and then add the layer out of the source
-map.addSource('covidcases', {
-    type: 'geojson',
-    data: 'assets/us-covid-2020-counts.json'
-});
-
-map.addLayer({
-        'id': 'covid-layer',
-        'type': 'circle',
-        'source': 'covidcases',
-        'minzoom': 4.5,
-        'paint': {
-            // increase the radii of the circle as the zoom level and dbh value increases
-            'circle-radius': {
-                'property': 'cases',
-                'stops': [
-                    [{
-                        zoom: 5,
-                        value: grades[0]
-                    }, radii[0]],
-                    [{
-                        zoom: 5,
-                        value: grades[1]
-                    }, radii[1]],
-                    [{
-                        zoom: 5,
-                        value: grades[2]
-                    }, radii[2]],
-                    [{
-                        zoom: 5,
-                        value: grades[3]
-                    }, radii[3]]
-                ]
-            },
-            'circle-color': {
-                'property': 'cases',
-                'stops': [
-                    [grades[0], colors[0]],
-                    [grades[1], colors[1]],
-                    [grades[2], colors[2]],
-                    [grades[3], colors[3]],
-                ]
-            },
-            'circle-stroke-color': 'white',
-            'circle-stroke-width': 1,
-            'circle-opacity': 0.6
-        }
-    },
-    'waterway-label'
-);
-
-
-// click on tree to view magnitude in a popup
-map.on('click', 'covid-layer', (event) => {
-    new mapboxgl.Popup()
-        .setLngLat(event.features[0].geometry.coordinates)
-        .setHTML(`<strong># of Cases:</strong> ${event.features[0].properties.cases}`)
-        .addTo(map);
-});
-
-});
-
-
-// create legend
-const legend = document.getElementById('legend');
-
-//set up legend grades and labels
-var labels = ['<strong>Cases</strong>'], vbreak;
-//iterate through grades and create a scaled circle and label for each
-for (var i = 0; i < grades.length; i++) {
-vbreak = grades[i];
-// you need to manually adjust the radius of each dot on the legend 
-// in order to make sure the legend can be properly referred to the dot on the map.
-dot_radii = 2 * radii[i];
-labels.push(
-    '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
-    'px; height: ' +
-    dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
-    '</span></p>');
-
-}
-const source =
-'<p style="text-align: right; font-size:10pt">Source: <a href="https://earthquake.usgs.gov/earthquakes/">USGS</a></p>';
-
-legend.innerHTML = labels.join('') + source;
+        // when loading a geojson, there are two steps
+        // add a source of the data and then add the layer out of the source
+        map.addSource('covidrates', {
+            type: 'geojson',
+            data: 'assets/us-covid-2020-rates.json'
+        });
+            
+        map.addLayer({
+            'id': 'covidrates-layer',
+            'type': 'fill',
+            'source': 'covidrates',
+            'paint': {
+                'fill-color': [
+                    'step',
+                    ['get', 'rates'],
+                    '#FFEDA0',   // stop_output_0
+                    20,          // stop_input_0
+                    '#FED976',   // stop_output_1
+                    40,          // stop_input_1
+                    '#FEB24C',   // stop_output_2
+                    60,          // stop_input_2
+                    '#FD8D3C',   // stop_output_3
+                    80,         // stop_input_3
+                    '#FC4E2A',   // stop_output_4
+                    100,         // stop_input_4
+                    '#E31A1C',   // stop_output_5
+                ],
+                'fill-outline-color': '#BBBBBB',
+                'fill-opacity': 0.7,
+            }
+        });
+    
+    
+        // click on tree to view magnitude in a popup
+        map.on('mousemove', ({point}) => {
+            const state = map.queryRenderedFeatures(point, {
+                layers: ['covidrates-layer']
+            });
+            document.getElementById('text-description').innerHTML = state.length ?
+                `<t>The county of </t>${state[0].properties.county}<t> has a </t><strong><em>${state[0].properties.rates}</strong> rate of covid</em>` :
+                `<p>Hover over a County!</p>`;
+        });
+    });
+    
+    
+    // create legend for choropleth map
+    const layers = [
+        '0-19',
+        '20-39',
+        '40-59',
+        '60-79',
+        '80-100'
+    ];
+    const colors = [
+        '#FFEDA070',
+        '#FED97670',
+        '#FEB24C70',
+        '#FD8D3C70',
+        '#FC4E2A70'
+    ];
+    const legend = document.getElementById('legend');
+    legend.innerHTML = "<b>Covid-19 Rates</b>";
+    
+    layers.forEach((layer, i) => {
+        const color = colors[i];
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+    
+        const value = document.createElement('span');
+        value.innerHTML = `${layer}`;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
+    });
